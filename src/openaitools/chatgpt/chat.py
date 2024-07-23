91a4ed4e1b18d1f2abfcdd41794f3f456d3738db
Adriano Gil <adrianogil.san@gmail.com>
2024-07-22 23:44:03 -0400

    feat: using gpt-4o-mini by default

--- src/openaitools/chatgpt/chat.py
+++ src/openaitools/chatgpt/chat.py
@@ -5,11 +5,11 @@ client = OpenAI(
     api_key=os.environ["CHATGPT_API_KEY"],
 )
 
-def get_chatgpt_output(user_input="Hello world!", messages=None):
+def get_chatgpt_output(user_input="Hello world!", messages=None, model='gpt-4o-mini'):
     if not messages:
         messages = []
     messages.append({"role": "user", "content": user_input})
-    completion = client.chat.completions.create(model="gpt-3.5-turbo", messages=messages)
+    completion = client.chat.completions.create(model=model, messages=messages)
     chatgpt_output = completion.choices[0].message.content
 
     return chatgpt_output
